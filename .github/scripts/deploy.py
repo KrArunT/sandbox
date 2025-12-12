890314665b6cd7f1fe1534fc01c5c1c157b17361
KrArunT <aruntiwary@infobellit.com>
2025-12-12 13:19:58 +0530

    Initial

---
+++ .github/scripts/deploy.py
@@ -0,0 +1,22 @@
+import os
+from huggingface_hub import HfApi
+
+token = os.environ["HF_TOKEN"]
+repo_id = "ArunKr/sandbox"
+
+api = HfApi(token=token)
+
+# Ensure the repository exists
+api.create_repo(
+    repo_id=repo_id,
+    repo_type="space",
+    exist_ok=True,
+    space_sdk="static"
+)
+
+# Upload the folder
+api.upload_folder(
+    repo_id=repo_id,
+    folder_path=".",
+    repo_type="space",
+)
